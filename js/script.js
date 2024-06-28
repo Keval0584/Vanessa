09c0f2023674fee09512a7e5852070c34653c45f
Keval0584 <keval0584@gmail.com>
2024-06-28 18:35:24 +0530

    first

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('#s1').owlCarousel({
+        loop:true,
+        // margin:10,
+        nav:false,
+        dots:true,
+        itmes:3,
+        dotsEach:2,
+        mouseDrag:false,
+        smartSpeed:1000,
+    })
+
+});
